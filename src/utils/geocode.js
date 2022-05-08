bc7cb320034e340dc15ed99048a42fe36cf63733
Sourav <souravghosh.neo@gmail.com>
2022-05-08 11:59:12 +0530

    error handled

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,10 +1,13 @@
 const request = require('request')
 const geocode =(location,cb)=>{
-    const url ='http://api.positionstack.com/v1/forward?access_key=0a255eb7ea0cb5afa1114c857286bbfc&query='+location
+    const url ='http://api.positionstack.com/v1/forward?access_key=0a255eb7ea0cb5afa1114c857286bbfc&query='+encodeURI(location)
     request({url , json:true},(error, response)=>{
         if (error){
             cb("Unable to connect to the location",undefined)
-        }else if(response.body.data.length===0){
+        }else if (response.body.error){
+            cb(response.body.error.context.query.message,undefined)
+        }
+        else if(response.body.data.length === 0){
             cb("Unable to find the location!",undefined)
 
         }
